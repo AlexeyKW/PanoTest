1bb00a60b2823782ae14dec6cfc726f9885dc341
Alexey Kolesnikow <alekseykw@gmail.com>
2020-09-09 17:23:38 +0700

    change initial pano

--- data.js
+++ data.js
@@ -1,7 +1,45 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-img_5940---img_5952_blended_fused",
+      "id": "0-img_5955---img_5969_fused",
+      "name": "IMG_5955 - IMG_5969_fused",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.927441732521018,
+        "pitch": -0.061302344989947244,
+        "fov": 1.1490439151539336
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.101083423710559,
+          "pitch": 0.08449550314358234,
+          "rotation": 0,
+          "target": "2-img_5832---img_5854_fused"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_5940---img_5952_blended_fused",
       "name": "IMG_5940 - IMG_5952_blended_fused",
       "levels": [
         {
@@ -32,7 +70,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "1-img_5832---img_5854_fused",
+      "id": "2-img_5832---img_5854_fused",
       "name": "IMG_5832 - IMG_5854_fused",
       "levels": [
         {
@@ -64,13 +102,13 @@ var APP_DATA = {
           "yaw": 3.012258053642502,
           "pitch": 0.011960546915387837,
           "rotation": 0,
-          "target": "2-img_5857---img_5873_fused"
+          "target": "3-img_5857---img_5873_fused"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-img_5857---img_5873_fused",
+      "id": "3-img_5857---img_5873_fused",
       "name": "IMG_5857 - IMG_5873_fused",
       "levels": [
         {
@@ -102,13 +140,13 @@ var APP_DATA = {
           "yaw": -0.2267432053989964,
           "pitch": 0.05028708701445339,
           "rotation": 0,
-          "target": "3-img_5892---img_5904_blended_fused"
+          "target": "4-img_5892---img_5904_blended_fused"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-img_5892---img_5904_blended_fused",
+      "id": "4-img_5892---img_5904_blended_fused",
       "name": "IMG_5892 - IMG_5904_blended_fused",
       "levels": [
         {
@@ -140,19 +178,19 @@ var APP_DATA = {
           "yaw": -1.7829873082681118,
           "pitch": 0.052546374917872285,
           "rotation": 0,
-          "target": "5-img_5923---img_5937_fused"
+          "target": "6-img_5923---img_5937_fused"
         },
         {
           "yaw": 1.7175332849990168,
           "pitch": 0.09985145655585725,
           "rotation": 0,
-          "target": "0-img_5940---img_5952_blended_fused"
+          "target": "1-img_5940---img_5952_blended_fused"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-img_5908---img_5920_blended_fused",
+      "id": "5-img_5908---img_5920_blended_fused",
       "name": "IMG_5908 - IMG_5920_blended_fused",
       "levels": [
         {
@@ -184,19 +222,19 @@ var APP_DATA = {
           "yaw": -1.356210901933542,
           "pitch": 0.07949196112536328,
           "rotation": 0,
-          "target": "3-img_5892---img_5904_blended_fused"
+          "target": "4-img_5892---img_5904_blended_fused"
         },
         {
           "yaw": -1.8283607050531288,
           "pitch": 0.0669684113145621,
           "rotation": 0,
-          "target": "5-img_5923---img_5937_fused"
+          "target": "6-img_5923---img_5937_fused"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-img_5923---img_5937_fused",
+      "id": "6-img_5923---img_5937_fused",
       "name": "IMG_5923 - IMG_5937_fused",
       "levels": [
         {
@@ -224,44 +262,6 @@ var APP_DATA = {
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-img_5955---img_5969_fused",
-      "name": "IMG_5955 - IMG_5969_fused",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 3.1394106663701127,
-        "pitch": -0.019438705205612195,
-        "fov": 1.2634123859167636
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.101083423710559,
-          "pitch": 0.08449550314358234,
-          "rotation": 0,
-          "target": "1-img_5832---img_5854_fused"
-        }
-      ],
       "infoHotspots": []
     }
   ],
